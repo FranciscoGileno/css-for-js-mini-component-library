1c104e654f303a8ad759949697e1971cf3e5c0d4
Gileno Oliveira <gileno.oliveira@incognia.com>
2022-12-08 23:00:33 -0300

    feat(select): Add select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalWrapper>
+        {displayedValue}
+        <IconWrapper id="chevron-down" strokeWidth={1} size={24} />
+      </PresentationalWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const PresentationalWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border: 0;
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  color: ${COLORS.gray700};
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+const IconWrapper = styled(Icon)`
+  margin-left: 8px;
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  pointer-events: none;
+`;
+
+const NativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  &:hover + div {
+    color: ${COLORS.black};
+  }
+
+  &:focus + div {
+    outline: 2px solid ${COLORS.primary};
+  }
+`;
 
 export default Select;
